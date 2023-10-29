d344d789968389c35a43a480769fe85ff54ac4cf
Zuber Shekh <zuber.shekh7@gmail.com>
2023-10-29 21:28:09 +0530

    downgraded the routes configurations to the v5 pattern

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -7,15 +7,31 @@ import "react-toastify/dist/ReactToastify.css";
 // components
 import Header from "./components/common/Header";
 
+// pages
+import Home from "../src/pages/common/Home";
+import Login from "../src/pages/common/Login";
+import Dashboard from "../src/pages/common/Dashboard";
+import Events from "../src/pages/common/Events";
+import NotFound from "../src/pages/common/NotFound";
+import About from "../src/pages/common/About";
+
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <ToastContainer />
-        <Outlet />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/about" element={<Events />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
-    </>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,54 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import Home from "./pages/common/Home";
-import About from "./pages/common/About";
-import NotFound from "./pages/common/NotFound";
-import Login from "./pages/common/Login";
-import Events from "./pages/common/Events";
-import Dashboard from "./pages/common/Dashboard";
+
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/auth/login",
-        element: <Login />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "/events/",
-        element: <Events />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
-
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <App />
   </Provider>
 );
